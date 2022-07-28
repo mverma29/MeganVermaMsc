4ddcb472d0ec802a74ff05618a9152d572e88500
Sam Clifford <sj.clifford@gmail.com>
2022-07-28 09:50:54 +0100

    Create mscprojectscript_00_main_script.R
    Main script to call all the others

---
+++ mscprojectscript_00_main_script.R
@@ -0,0 +1,10 @@
+# Megan Verman MSc Project
+
+# Load all packages required for analysis
+source("mscprojectscript_01_load_packages.R")
+
+# Load RESPICAR data and augment with national level variables
+source("mscprojectscript_02_load_data.R")
+
+# Exploratory data analysis
+source("mscprojectscript_03_explore_data.R")
