6687e48da8138638c455e38aebfd6f9fd047ecda
Sam Clifford <sj.clifford@gmail.com>
2022-08-01 15:52:25 +0100

    adding new caledonia urbanicity

--- mscprojectscript_02_load_data.R
+++ mscprojectscript_02_load_data.R
@@ -17,33 +17,55 @@ urban_percent <- read_csv("data/urban_pop_percent.csv")  %>%
                  values_to = "urban_percent") %>%
     mutate(year = parse_integer(year))
 
-# Taiwan urban population
+# Taiwan and New Caledonia urban population
 # https://www.worldometers.info/world-population/taiwan-population/
+# https://www.worldometers.info/world-population/new-caledonia-population/
 # using UN WPP 2019
 
-urban_percent_twn <-
-    tibble(year = c(seq(1980, 2015, by = 5),
-                    seq(2016, 2020, by = 1)),
-           urban_percent = c(48.2,
-                             57.2,
-                             65.7,
-                             68.0,
-                             69.5,
-                             72.0,
-                             74.4,
-                             76.7,
-                             77.1,
-                             77.6,
-                             78.0,
-                             78.5,
-                             78.9),
-           iso_code = 158L,
-           `Country Code` = "TWN",
-           `Country Name` = "Taiwan",
-           `Indicator Name` = "Urban population (% of total population)",
-           `Indicator Code` = "SP.URB.TOTL.IN.ZS")
+urban_percent_extra <-
+    list(`158` = 
+             tibble(year = c(seq(1980, 2015, by = 5),
+                             seq(2016, 2020, by = 1)),
+                    urban_percent = c(48.2,
+                                      57.2,
+                                      65.7,
+                                      68.0,
+                                      69.5,
+                                      72.0,
+                                      74.4,
+                                      76.7,
+                                      77.1,
+                                      77.6,
+                                      78.0,
+                                      78.5,
+                                      78.9),
+                    `Country Code` = "TWN",
+                    `Country Name` = "Taiwan",
+                    `Indicator Name` = "Urban population (% of total population)",
+                    `Indicator Code` = "SP.URB.TOTL.IN.ZS"),
+         `540` = tibble(year = c(seq(1980, 2015, by = 5),
+                                 seq(2016, 2020, by = 1)),
+                        urban_percent = c(57.4,
+                                          58.8, 
+                                          59.4,
+                                          59.5,
+                                          60.7,
+                                          62.9,
+                                          66.4,
+                                          68.9,
+                                          69.4,
+                                          70.0,
+                                          70.6, 
+                                          71.3,
+                                          71.9),
+                        `Country Code` = "NCL",
+                        `Country Name` = "New Caledonia",
+                        `Indicator Name` = "Urban population (% of total population)",
+                        `Indicator Code` = "SP.URB.TOTL.IN.ZS")) %>%
+    bind_rows(.id = "iso_code") %>%
+    mutate(iso_code = parse_integer(iso_code))
 
-urban_percent %<>% bind_rows(urban_percent_twn)
+urban_percent %<>% bind_rows(urban_percent_extra)
 
 # GDP----
 WDIsearch("gdp per capita")
