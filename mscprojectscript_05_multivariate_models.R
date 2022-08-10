335b909b3035e4ff753f318769ae030ee8068c37
Megan Verma <megan.verma@gmail.com>
2022-08-10 17:35:34 +0100

    carriage variable in models, formatting

--- mscprojectscript_05_multivariate_models.R
+++ mscprojectscript_05_multivariate_models.R
@@ -1,22 +1,32 @@
 # full model (without interaction), with RE for subregion correlation----
 
 full_glm <- glmer(
-    data = respicar_socio %>% 
-        mutate(p = Positive/Total),
-    formula = p ~ urban_percent + log_gdp + gini + mean_hh + female_ed + (1|subregion),
-    family  = "binomial", 
-    weights = Total)
+    data = respicar_socio,
+    formula = carriage ~ urban_percent + log_gdp + gini + mean_hh + female_ed + (1|subregion),
+    family  = "binomial",
+    weights = Total
+)
 
-summ(full_glm, exp=TRUE, confint=TRUE)  #only urban_percent (barely) and mean_hh have ORs other than 1
+summ(
+    full_glm,
+    exp = TRUE,
+    confint = TRUE,
+    pvals = TRUE,
+    digits = 3
+) 
 
 # full model (with interaction), with RE for subregion correlation----
 
-# check formatting for interaction term in lme4- cause of error
 full_glm_interaction <- glmer(
-    data = respicar_socio %>% 
-        mutate(p = Positive/Total),
-    formula = p ~ urban_percent + log_gdp*gini + mean_hh + female_ed + (1|subregion),
+    data    = respicar_socio,
+    formula = carriage ~ urban_percent + log_gdp*gini + mean_hh + female_ed + (1|subregion),
     family  = "binomial", 
     weights = Total)
 
-summ(full_glm_interaction, exp=TRUE, confint=TRUE)  #only urban_percent (barely) and mean_hh have ORs other than 1
+summ(
+    full_glm_interaction,
+    exp = TRUE,
+    confint = TRUE,
+    pvals = TRUE,
+    digits = 3
+) 
