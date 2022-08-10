7c471885cfb077991a4b6ec1c8d55ebb21a65b5b
Megan Verma <megan.verma@gmail.com>
2022-08-10 01:28:25 +0100

    fixed effects in full models

--- mscprojectscript_05_multivariate_models.R
+++ mscprojectscript_05_multivariate_models.R
@@ -1,14 +1,22 @@
 # full model (without interaction), with RE for subregion correlation----
 
-# add in RE 
-full_glm <- glm(data    = respicar_socio %>% mutate(p = Positive/Total),
-                formula = p ~ urban_percent + gdp + gini + mean_hh + female_ed,
-                family  = "binomial", weights = Total) 
+full_glm <- glmer(
+    data = respicar_socio %>% 
+        mutate(p = Positive/Total),
+    formula = p ~ urban_percent + gdp_usd + gini + mean_hh + female_ed + (1|subregion),
+    family  = "binomial", 
+    weights = Total)
+
+summ(full_glm, exp=TRUE, confint=TRUE)  #only urban_percent (barely) and mean_hh have ORs other than 1
 
 # full model (with interaction), with RE for subregion correlation----
 
-# add in RE 
-# check formatting for interaction term (*??)
-full_glm <- glm(data    = respicar_socio %>% mutate(p = Positive/Total),
-                formula = p ~ urban_percent + gdp*gini + mean_hh + female_ed,
-                family  = "binomial", weights = Total) 
+# check formatting for interaction term in lme4- cause of error
+full_glm_interaction <- glmer(
+    data = respicar_socio %>% 
+        mutate(p = Positive/Total),
+    formula = p ~ urban_percent + gdp_usd*gini + mean_hh + female_ed + (1|subregion),
+    family  = "binomial", 
+    weights = Total)
+
+summ(full_glm_interaction, exp=TRUE, confint=TRUE)  #only urban_percent (barely) and mean_hh have ORs other than 1
