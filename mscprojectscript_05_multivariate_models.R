4d13a7f42b392754c9bd5f2fd7f0c3471d7ea155
Megan Verma <megan.verma@gmail.com>
2022-08-10 18:09:48 +0100

    fixing the merge i did :(

--- mscprojectscript_05_multivariate_models.R
+++ mscprojectscript_05_multivariate_models.R
@@ -1,20 +1,7 @@
 # full model (without interaction), with RE for subregion correlation----
 
 full_glm <- glmer(
-<<<<<<< HEAD
-    data = respicar_socio %>% 
-        mutate(p = Positive/Total),
-    formula = p ~ urban_percent + log_gdp + gini + mean_hh + female_ed + (1|subregion),
-    family  = "binomial", 
-    weights = Total)
-
-summ(full_glm, exp=TRUE, confint=TRUE)  #only urban_percent (barely) and mean_hh have ORs other than 1
-=======
-<<<<<<< HEAD
     data    = respicar_socio,
-=======
-    data = respicar_socio,
->>>>>>> master
     formula = carriage ~ urban_percent + log_gdp + gini + mean_hh + female_ed + (1|subregion),
     family  = "binomial",
     weights = Total
@@ -22,45 +9,26 @@ summ(full_glm, exp=TRUE, confint=TRUE)  #only urban_percent (barely) and mean_hh
 
 summ(
     full_glm,
-<<<<<<< HEAD
     exp     = TRUE,
     confint = TRUE,
     pvals   = TRUE,
     digits  = 3
-=======
-    exp = TRUE,
-    confint = TRUE,
-    pvals = TRUE,
-    digits = 3
->>>>>>> master
 ) 
->>>>>>> master
 
 # full model (with interaction), with RE for subregion correlation----
 
 # check formatting for interaction term in lme4- cause of error
 full_glm_interaction <- glmer(
-    data = respicar_socio %>% 
-        mutate(p = Positive/Total),
-    formula = p ~ urban_percent + log_gdp*gini + mean_hh + female_ed + (1|subregion),
-    family  = "binomial", 
-    weights = Total)
+    data = respicar_socio,
+    formula = carriage ~ urban_percent + log_gdp * gini + mean_hh + female_ed + (1|subregion),
+    family  = "binomial",
+    weights = Total
+)
 
-<<<<<<< HEAD
-summ(full_glm_interaction, exp=TRUE, confint=TRUE)  #only urban_percent (barely) and mean_hh have ORs other than 1
-=======
 summ(
     full_glm_interaction,
-<<<<<<< HEAD
     exp     = TRUE,
     confint = TRUE,
     pvals   = TRUE,
     digits  = 3
-=======
-    exp = TRUE,
-    confint = TRUE,
-    pvals = TRUE,
-    digits = 3
->>>>>>> master
 ) 
->>>>>>> master
